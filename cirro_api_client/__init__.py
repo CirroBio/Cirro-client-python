3a3148ae3b326a40147bb1c9ebfb27cf85ec9e33
Nathan Thorpe <nathan@techstormpc.com>
2024-01-29 12:04:41 -0800

    fix lint

--- cirro_api_client/__init__.py
+++ cirro_api_client/__init__.py
@@ -1,4 +1,4 @@
-from .cirro_auth import TokenAuth, RefreshableTokenAuth
+from .cirro_auth import RefreshableTokenAuth, TokenAuth
 from .cirro_client import CirroApiClient
 
 __all__ = ("CirroApiClient", "TokenAuth", "RefreshableTokenAuth")

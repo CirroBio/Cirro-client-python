de863e49662517dd68f97278d6d215fd95b1bf5b
Nathan Thorpe <nathan@techstormpc.com>
2025-03-25 13:15:09 -0700

    Add descriptions to enum

--- cirro_api_client/v1/models/share_type.py
+++ cirro_api_client/v1/models/share_type.py
@@ -3,8 +3,11 @@ from enum import Enum
 
 class ShareType(str, Enum):
     AVAILABLE = "AVAILABLE"
+    """ The share is available for subscription """
     PUBLISHER = "PUBLISHER"
+    """ The project owns this share """
     SUBSCRIBER = "SUBSCRIBER"
+    """ The project can view this share """
     UNKNOWN = "UNKNOWN"
     """ This is a fallback value for when the value is not known, do not use this value when making requests """
 

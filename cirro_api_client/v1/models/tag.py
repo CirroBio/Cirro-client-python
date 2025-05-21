2954344d33fc6b7d67d4b7f6969952eb1f2184a8
Nathan Thorpe <nathan@techstormpc.com>
2025-05-21 16:34:59 -0700

    CI-630 - Make editable and key not required in tag (#12)

--- cirro_api_client/v1/models/tag.py
+++ cirro_api_client/v1/models/tag.py
@@ -1,7 +1,9 @@
-from typing import Any, Dict, List, Type, TypeVar
+from typing import Any, Dict, List, Type, TypeVar, Union, cast
 
 from attrs import define as _attrs_define
 from attrs import field as _attrs_field
+
+from ..types import UNSET, Unset
 
 T = TypeVar("T", bound="Tag")
 
@@ -10,48 +12,61 @@ T = TypeVar("T", bound="Tag")
 class Tag:
     """
     Attributes:
-        key (str):
-        value (str):
-        editable (bool):
+        value (str): The value of the tag
+        editable (Union[Unset, bool]): Whether the tag value is editable Default: True.
+        key (Union[None, Unset, str]):
     """
 
-    key: str
     value: str
-    editable: bool
+    editable: Union[Unset, bool] = True
+    key: Union[None, Unset, str] = UNSET
     additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
 
     def to_dict(self) -> Dict[str, Any]:
-        key = self.key
-
         value = self.value
 
         editable = self.editable
+
+        key: Union[None, Unset, str]
+        if isinstance(self.key, Unset):
+            key = UNSET
+        else:
+            key = self.key
 
         field_dict: Dict[str, Any] = {}
         field_dict.update(self.additional_properties)
         field_dict.update(
             {
-                "key": key,
                 "value": value,
-                "editable": editable,
             }
         )
+        if editable is not UNSET:
+            field_dict["editable"] = editable
+        if key is not UNSET:
+            field_dict["key"] = key
 
         return field_dict
 
     @classmethod
     def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
         d = src_dict.copy()
-        key = d.pop("key")
-
         value = d.pop("value")
 
-        editable = d.pop("editable")
+        editable = d.pop("editable", UNSET)
+
+        def _parse_key(data: object) -> Union[None, Unset, str]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, str], data)
+
+        key = _parse_key(d.pop("key", UNSET))
 
         tag = cls(
-            key=key,
             value=value,
             editable=editable,
+            key=key,
         )
 
         tag.additional_properties = d
